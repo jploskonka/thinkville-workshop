77d66282ae32cc3990e3a41e4d0d34431e764f22
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-04-27 14:08:14 +0200

    feat: position sliders under painting

--- sketch.js
+++ sketch.js
@@ -2,12 +2,24 @@ let redSlider,
   greenSlider,
   blueSlider;
 
-function setup() {
-  createCanvas(500, 500);
+const TEXT_SIZE = 16;
+const PAINTING_HEIGHT = 500;
+const PAINTING_WIDTH = 500;
 
-  redSlider   = createSlider(0, 255);
-  greenSlider = createSlider(0, 255);
-  blueSlider  = createSlider(0, 255);
+const sliderWithLabel = (label, x, y) => {
+  text(label, x, y);
+
+  return createSlider(0, 255)
+    .position(x, y + TEXT_SIZE);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(TEXT_SIZE);
+
+  redSlider   = sliderWithLabel("red", 30, PAINTING_HEIGHT + 50);
+  greenSlider = sliderWithLabel("green", 30, PAINTING_HEIGHT + 100);
+  blueSlider  = sliderWithLabel("blue", 30, PAINTING_HEIGHT + 150);
 }
 
 function draw() {
