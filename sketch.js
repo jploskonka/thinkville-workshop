f59b8ea3f1aa458a6237035d55ca7232ebecc008
Jakub Płoskonka <ploskonkajakub@gmail.com>
2020-05-01 07:36:50 +0200

    feat: replace control sliders with randomized values

--- sketch.js
+++ sketch.js
@@ -1,19 +1,9 @@
-let redSlider,
-  greenSlider,
-  blueSlider,
-  countSlider;
-
 let paintingHeight,
   paintingWidth;
 
 const TEXT_SIZE = 16;
 
-const sliderWithLabel = (label, x, y) => {
-  text(label, x, y);
-
-  return createSlider(0, 255)
-    .position(x, y + TEXT_SIZE);
-}
+let red, green, blue, count;
 
 function setup() {
   paintingWidth = windowWidth;
@@ -22,24 +12,17 @@ function setup() {
   let canvas = createCanvas(windowWidth, paintingHeight);
   canvas.parent('header');
 
-  textSize(TEXT_SIZE);
+  red = random(0, 255)
+  green = random(0, 255);
+  blue = random(0, 255);
 
-  redSlider   = sliderWithLabel("red", 30, paintingHeight + 50);
-  greenSlider = sliderWithLabel("green", 30, paintingHeight + 100);
-  blueSlider  = sliderWithLabel("blue", 30, paintingHeight + 150);
-  countSlider = sliderWithLabel("count", 30, paintingHeight + 200);
+  count = random(5, 30);
 }
 
 function draw() {
-  const red = redSlider.value();
-  const green = greenSlider.value();
-  const blue = blueSlider.value();
-
-  const count = countSlider.value();
-
   fill(red, green, blue);
-  for(let i = 0; i <= count; i++) {
-    for(let j = 0; j <= count; j++) {
+  for(let i = 0; i <= count + 1; i++) {
+    for(let j = 0; j <= count + 1; j++) {
       const ellipseWidth = paintingWidth/count;
 
       ellipse(ellipseWidth * i, ellipseWidth * j, ellipseWidth);
